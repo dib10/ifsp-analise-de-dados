24d48f647223a2db6f5ee9759c2165c063da3149
CTI Guarulhos <ctiguarulhos@ifsp.edu.br>
2024-06-07 20:34:56 -0300

    media

---
+++ aula07junho.R
@@ -0,0 +1,16 @@
+v = c(6,2,3,5,2)
+
+# precisamos de contador e de acumulador
+
+cont = 0
+acum = 0
+#o cálculo é executado em um laço
+
+for(x in v)
+{
+  acum <- acum + x
+  cont <- cont + 1
+  
+}
+
+media = acum / cont
